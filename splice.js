624ca253acf8d32a59a2c0050c640a4ceb8d6033
Kapil Sabharwal <kapilsabharwalwins@gmail.com>
2023-03-24 16:24:28 +0530

    added splice

---
+++ splice.js
@@ -0,0 +1,19 @@
+//The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
+
+//syntax:
+//splice(start)
+//splice(start, deleteCount)
+//splice(start, deleteCount, item1)
+//splice(start, deleteCount, item1, item2, itemN)
+
+//Example:
+const myFish = ["angel", "clown", "mandarin", "sturgeon"];
+const removed = myFish.splice(2, 0, "drum");
+
+console.log(removed); // [], no elements removed
+console.log(myFish); // ["angel", "clown", "drum", "mandarin", "sturgeon"]
+
+//Example:
+const removed2 = myFish.splice(3, 1);
+console.log(removed2); // ["mandarin"]
+console.log(myFish); // ["angel", "clown", "drum", "sturgeon"]
