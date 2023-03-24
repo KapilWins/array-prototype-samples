263350f5dcc708c8e7ceee1aed8cd03cf29a4a12
Kapil Sabharwal <kapilsabharwalwins@gmail.com>
2023-03-24 09:50:01 +0530

    added .at()

---
+++ at.js
@@ -0,0 +1,15 @@
+//The at() method takes an integer value and returns the item at that index, allowing for positive and negative integers. Negative integers count back from the last item in the array.
+
+//syntax:
+//at(index)
+
+const cart = ["apple", "banana", "pear"];
+
+console.log(cart.at(0)); //'apple'
+
+console.log(cart.at(-1)); //'pear'
+
+// Add an item to our 'cart' array
+cart.push("orange"); //["apple", "banana", "pear","orange"]
+
+console.log(cart.at(-1)); // 'orange'
